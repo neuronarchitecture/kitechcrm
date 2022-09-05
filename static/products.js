8a113bbc686cacc56ed3b5fa18bcf12ff4bee551
Ehmud bk <dev@neuronarchitecture.com>
2022-09-05 03:44:07 +0200

    adding parts to the products and new buttons UI

--- static/products.js
+++ static/products.js
@@ -40,7 +40,11 @@ const usertest = document.getElementById("usertest");
 const partsName = document.querySelector('.partsName')
 const editpartsName = document.querySelector('.editpartsName')
 const materiallist = document.querySelector('.materiallist')
+const partslist = document.querySelector('.partslist')
 const subslist = document.querySelector('.subslist')
+const supplierinfo = document.querySelector('#supplierinfo')
+const partname = document.querySelector("#partname")
+const checkpartdata = document.querySelector('.checkpartdata')
 
 let id;
 
@@ -65,17 +69,39 @@ const renderUser = doc => {
       <td>${doc.data().productSpecs}</td>
       <td>${doc.data().productWeight}</td>
         <td>${doc.data().productNumber}</td>
-      <td>
 
-    <button type="button" class="viewbtn btn btn-primary mr-2 " data-toggle="modal" data-target="#exampleModalScrollable">view</button>
-        <button class="adminelement btnpr-edit"> <a class="badge bg-success mr-2" class="btn btn-primary mt-2" data-toggle="modal" data-target="#exampleModalScrollableEDIT" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit" href="#" ><i class="ri-pencil-line mr-0"></i></a></button>
-      
-         
-        <button class=" btnpr-delete"><a class="badge bg-warning mr-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="#"><i class="ri-delete-bin-line mr-0"></i></a></button>
+
+    <td>
+    <div class="btngroup">
+     <span href="#" class="button btn-large viewbtn" data-toggle="modal" data-target="#exampleModalScrollable">View </span>
+     <div class="button-dropdown">
+        <a class="button toggle"></a>
+        <ul class="dropdown">
+          <li><a href="#" class="dropdown-link btnpr-edit">Edit</a></li>
+          <li><a href="#" class="dropdown-link btnpr-delete">Delete</a></li>
+          <li><a href="#" class="dropdown-link">Download</a></li>
+        </ul>
+      </div>
+    </div>
+   
       </td>
     </tr>
   `;
   tableUsers.insertAdjacentHTML('beforeend', tr);
+
+
+// var productsRef = db.collection("recycledproducts");
+// productsRef.where("productName", "==", "testparts")
+// .get()
+// .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             // doc.data() is never undefined for query doc snapshots
+//             console.log(doc.id, " => ", doc.data());
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("Error getting documents: ", error);
+//     });
 
 //add parts
 //  const btnpraddSubstances = document.querySelector(`[data-id='${doc.id}'] .btnpr-addSubs`);
@@ -115,48 +141,50 @@ const renderUser = doc => {
 
 // Click on view product button
 
-  const viewbtn = document.querySelector(`[data-id='${doc.id}'] .viewbtn`);
-  viewbtn.addEventListener('click', ()=> {
+  const viewpartspr = document.querySelector(`[data-id='${doc.id}'] .viewbtn`);
+  viewpartspr.addEventListener('click', ()=> {
 
     //click add parts button
- const setupMaterialUI = (data) => {
-    let html = '';
-    data.forEach(doc=> {
-      const material = doc.data();
-      console.log(material)
-      const li = `
-       <tr data-id='${doc.id}'>
-      <td>${doc.data().materialGroup}</td>
-     <td>${doc.data().materialName}</td>
-      <td>${doc.data().materialClassId}</td>
-       <td>${doc.data().materialRecycleContent}</td>
-        <td>${doc.data().materialRecycleType}</td>
-         <td>${doc.data().materialMassg}</td>
-     <td>${doc.data().materialMassPerc}</td>
-      <td>
-        <div class="row">
+//  const setupPartsUI = (data) => {
+//     let html = '';
+//     data.forEach(doc=> {
+//       const material = doc.data();
+//       console.log(material)
+//       const li = `
+//        <tr data-id='${doc.id}'>
+//       <td>${doc.data().materialGroup}</td>
+//      <td>${doc.data().materialName}</td>
+//       <td>${doc.data().materialClassId}</td>
+//        <td>${doc.data().materialRecycleContent}</td>
+//         <td>${doc.data().materialRecycleType}</td>
+//          <td>${doc.data().materialMassg}</td>
+//      <td>${doc.data().materialMassPerc}</td>
+//       <td>
+//         <div class="row">
       
-      <button class="adminelement btnpr-edit"> <a class="badge bg-success mr-2" class="btn btn-primary mt-2" data-toggle="modal" data-target="#exampleModalScrollableEDIT" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit" href="#" ><i class="ri-pencil-line mr-0"></i></a></button>
+//       <button class="adminelement btnpr-edit"> <a class="badge bg-success mr-2" class="btn btn-primary mt-2" data-toggle="modal" data-target="#exampleModalScrollableEDIT" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit" href="#" ><i class="ri-pencil-line mr-0"></i></a></button>
        
-        <button class=" btnpr-delete"><a class="badge bg-warning mr-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="#"><i class="ri-delete-bin-line mr-0"></i></a></button>
-        </td>
-        </div>
+//         <button class=" btnpr-delete"><a class="badge bg-warning mr-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="#"><i class="ri-delete-bin-line mr-0"></i></a></button>
+//         </td>
+//         </div>
       
 
       
     
-    </tr>
-      `;
-      html+=li
-    })
+//     </tr>
+//       `;
+//       html+=li
+//     })
 
-    materiallist.insertAdjacentHTML('beforeend', html)
-  }
+//     partslist.insertAdjacentHTML('beforeend', html)
+  // }
   
-   db.collection('recycledproducts').doc(`${doc.id}`).collection('materials').get().then(snapshot => {
-    setupMaterialUI(snapshot.docs)
-   })
+  //  db.collection('recycledproducts').doc(`${doc.id}`).collection('materials').get().then(snapshot => {
+  //   setupMaterialUI(snapshot.docs)
+  //  })
 
+
+   
    
       // Get a reference from the dom to the tree elements
      const treeproducttitle = document.querySelector('.treeproducttitle')
@@ -183,7 +211,80 @@ const renderUser = doc => {
   })
 
 
+//add parts 
+ const btnpredit = document.querySelector(`[data-id='${doc.id}'] .btnpr-edit`);
+const addPart = document.querySelector('.addPart')
+btnpredit.addEventListener('click', () => {
+    console.log(doc.id, " => ", doc.data());
+   addPart.addEventListener('click', e => {
+  e.preventDefault();
 
+  db.collection('recycledproducts').doc(`${doc.id}`).collection('parts').add({
+    partNumber: partNumber.value,
+    partSpecs: partSpecs.value,
+    partMassgEA: partMassgEA.value,
+    partMassg: partMassg.value,
+    partMassPerc: partMassPerc.value,
+    reusedPart: partClass.value,
+    companyName: companyName.value,
+    
+  })
+  // materialtitle.innerHTML = `${doc.data().partName}`
+
+    //view parts list for a single product
+ 
+
+})
+})
+
+
+   btnpredit.addEventListener('click', () => {
+     const partslist = document.querySelector('.partslist')
+ partslist.innerHTML = "";
+ const setupPartsUI = (data) => {
+    let html = '';
+    data.forEach(doc=> {
+      const parts = doc.data();
+      console.log(parts)
+      const pl = `
+    <tr data-id='${doc.id}'>
+     <td><img src="./productimage.png" style="height: 60px;
+    width: 60px;" alt=""></td>
+     <td>${doc.data().companyName}</td>
+      <td>${doc.data().partMassPerc}</td>
+      <td>${doc.data().partMassg}</td>
+      <td>${doc.data().partMassgEA}</td>
+      <td>${doc.data().partNumber}</td>
+        <td>${doc.data().partSpecs}</td>
+         <td>${doc.data().reusedPart}</td>
+
+
+    <td>
+    <div class="btngroup">
+     <span href="#" class="button btn-large viewbtn" data-toggle="modal" data-target="#exampleModalScrollable">View </span>
+     <div class="button-dropdown">
+        <a class="button toggle"></a>
+        <ul class="dropdown">
+          <li><a href="#" class="dropdown-link btnpr-edit">Edit</a></li>
+          <li><a href="#" class="dropdown-link btnpr-delete">Delete</a></li>
+          <li><a href="#" class="dropdown-link">Download</a></li>
+        </ul>
+      </div>
+    </div>
+   
+      </td>
+    </tr>
+      `;
+      html+=pl
+    })
+
+    partslist.insertAdjacentHTML('beforeend', html)
+  }
+  
+   db.collection('recycledproducts').doc(`${doc.id}`).collection('parts').get().then(snapshot => {
+    setupPartsUI(snapshot.docs)
+   })
+    })
 
 
   // Click edit user
@@ -198,17 +299,12 @@ const renderUser = doc => {
     editmodalyForm.productClass.value = doc.data().productClass;
     editmodalyForm.productWeight.value = doc.data().productWeight;
     editmodalyForm.productNumber.value = doc.data().productNumber;
-    
     editmodalyForm.productCategory.value = doc.data().productCategory;
-    
-
-    
-    //edit the parts section
-    
-
-   
-
   });
+
+
+
+
 
   // Click delete user
   const btnprDelete = document.querySelector(`[data-id='${doc.id}'] .btnpr-delete`);
@@ -386,8 +482,8 @@ const editUI = (user) => {
       const userDisplayName = firebase.auth().currentUser.displayName
     console.log(user.metadata.lastSignInTime)
     console.log(firebase.auth().currentUser.displayName)
-    firstcreation.innerHTML = "Since "  + firstcreationtime;
-    userDisplay.innerHTML = userDisplayName;
+    // firstcreation.innerHTML = "Since "  + firstcreationtime;
+    // userDisplay.innerHTML = userDisplayName;
     } else {
        const userTitleCard = document.getElementById('usertitle')
        
@@ -411,6 +507,8 @@ const editUI = (user) => {
 }
 
 
+
+
  db.collection("recycledparts")
     .get()
     .then((querySnapshot) => {
@@ -430,5 +528,92 @@ const editUI = (user) => {
 
 
 
+  db.collection("recycledparts")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+           const tm = `
+      <option>${doc.data().supplierInfo}</option>
+  `;
+
+   supplierinfo.insertAdjacentHTML('beforeend', tm);
+ 
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+  
+
+    db.collection("recycledparts")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+           const tm = `
+      <option>${doc.data().partName}</option>
+  `;
+
+   partname.insertAdjacentHTML('beforeend', tm);
+ 
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+const partNumber = document.querySelector('#partNumber')
+const partSpecs = document.querySelector('#partSpecs')
+const partMassgEA = document.querySelector('#partMassgEA')
+const partMassg = document.querySelector('#partMassg')
+const partMassPerc = document.querySelector('#partMassPerc')
+const partClass = document.querySelector('#partClass')
+const companyName = document.querySelector('#companyName')
+
+checkpartdata.addEventListener('click', () => {
+ 
+  const partsRef = db.collection("recycledparts")
+    partsRef.where("supplierInfo", "==", supplierinfo.value).where("partName", "==", partname.value)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+          partNumber.value = doc.data().partNumber
+          partSpecs.value = doc.data().partSpecs
+          partMassgEA.value = doc.data().partMassgEA
+          partMassg.value = doc.data().partMassg
+          partMassPerc.value = doc.data().partMassPerc
+          partClass.value = doc.data().reusedPart
+          companyName.value = doc.data().companyName
+
+        });
+    })
+  
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+})
 
 
+
+
+
+
+//   var productsRef = db.collection("recycledparts");
+// productsRef.where("productName", "==", "testparts")
+// .get()
+// .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             // doc.data() is never undefined for query doc snapshots
+//             console.log(doc.id, " => ", doc.data());
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("Error getting documents: ", error);
+//     });
+
+// 
