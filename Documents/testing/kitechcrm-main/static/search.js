c80328e7f66dff79d5e3c87dfe861791e255c828
Ehmud bk <dev@neuronarchitecture.com>
2022-09-14 08:21:17 +0200

    added multiselect and search

--- Documents/testing/kitechcrm-main/static/search.js
+++ Documents/testing/kitechcrm-main/static/search.js
@@ -45,6 +45,29 @@ function materialSearch() {
         };
 
 
+        function productSearch() {
+            let input, filter, table, tr, td, txtValue;
+
+            //Intialising Variables
+            input = document.getElementById("myInputProd");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTableProd");
+            tr = table.getElementsByTagName("tr");
+
+            for (let i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+
+        };
+
         ///////////// Multiselect field
 
     let multiselect_block = document.querySelectorAll(".multiselect_block");
